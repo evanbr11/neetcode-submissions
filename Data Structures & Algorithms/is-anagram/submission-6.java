c8603b49dd131c01a968807cd182205bf0a50c3b
Evan Bray <bray.ev@northeastern.edu>
2026-06-18 12:27:21 -0400

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        s = s.toLowerCase();
+        t = t.toLowerCase();
+        
+        Map<Character, Integer> map = new HashMap<>(26);
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            map.put(ch, 0);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
+            map.put(t.charAt(i), map.get(t.charAt(i)) - 1);
+        }
+        if (map.values().stream().allMatch(n -> n == 0)) {
+            return true;
+        }
+        return false;
+    }
+}
