dd0701411b1240bb10bd12569ee88f0e428e5a16
Evan Bray <evanbray63@gmail.com>
2026-08-07 16:00:43 -0400

    Add: concatenation-of-array - submission-15

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-15.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] concat = new int[nums.length * 2];
+        for (int i = 0; i < nums.length; i++) {
+            concat[i] = nums[i];
+            concat[i + nums.length] = nums[i];
+        }
+        return concat;
+    }
+}
